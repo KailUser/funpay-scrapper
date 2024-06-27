407ef92eb9eaadd8453e95eb0e9ee91d826748a8
Syirezz <111554400+KailUser@users.noreply.github.com>
2024-06-27 15:31:15 +0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="funpay-scrapper",
-    version="0.1.2",
+    version="0.1.3",
     description="Funpay Scraper",
     long_description=long_description,
     long_description_content_type="text/markdown",
